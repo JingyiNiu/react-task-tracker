5de57123a20588d28e71f05c12096668a48d50ad
JingyiNiu <niu.jy1203@gmail.com>
2021-07-07 19:57:22 +1200

    style container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from "./components/header/header.component";
 import Footer from "./components/footer/footer.component";
 
+import "./App.css"
+
 function App() {
   return (
     <div className='container'>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./header.styles.css";
 
 const Header = () => {
   return (
