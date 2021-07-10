5eb4c06a7744f7b4b22fe9f0b5e3119b939437c9
JingyiNiu <niu.jy1203@gmail.com>
2021-07-10 19:26:09 +1200

    install json-server

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ import "./App.css";
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
 
-  const [tasks, setTasks] = useState([
-    { id: 1, text: "buy milk", day: "Feb 1st at 10:00am", reminder: false },
-    { id: 2, text: "go doctor", day: "Feb 2nd at 2:30pm", reminder: true },
-    { id: 3, text: "pick up Tom", day: "Feb 1st at 8:00pm", reminder: true },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   // Add task
   const addTask = (task) => {
