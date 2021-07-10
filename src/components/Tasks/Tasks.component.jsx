be2a86a2101e328e7315c347d4883677098c9192
JingyiNiu <niu.jy1203@gmail.com>
2021-07-10 12:57:07 +1200

    add task component and pass data to it

--- src/components/Tasks/Tasks.component.jsx
+++ src/components/Tasks/Tasks.component.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
+import Task from "../Task/Task.component";
 
-
-const Tasks = ({tasks}) => {
-  
-
+const Tasks = ({ tasks }) => {
   return (
     <div>
-      {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
-      ))}
+    {tasks.map((task,index)=>(<Task key={index} task={task}/>))}
     </div>
   );
 };
