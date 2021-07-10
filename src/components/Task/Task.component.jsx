3337d4de2d8df144fa168dc4ba1e77f6ec4e30ac
JingyiNiu <niu.jy1203@gmail.com>
2021-07-10 13:07:10 +1200

    install react-icons and import FaTimes icon

--- src/components/Task/Task.component.jsx
+++ src/components/Task/Task.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import { FaTimes } from "react-icons/fa";
+
 import "./Task.styles.css";
 
 const Task = ({ task }) => {
   return (
     <div className='task'>
-      <h3>{task.text}</h3>
+      <h3>
+        {task.text} <FaTimes style={{ color: "red", cursor: "pointer" }} />
+      </h3>
       <p>{task.day}</p>
     </div>
   );
